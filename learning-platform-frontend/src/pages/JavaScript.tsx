3709972afbf9ab843a84231c86b432272bd833a9
Altynai <harmoni5413@gmail.com>
2026-01-12 10:22:30 -0500

    Added JavaScript Fundamentals page

---
+++ learning-platform-frontend/src/pages/JavaScript.tsx
@@ -0,0 +1,17 @@
+export default function JavaScript() {
+  return (
+    <div>
+      <h2>JavaScript Fundamentals</h2>
+
+      <iframe
+        width="600"
+        height="350"
+        src="https://www.youtube.com/embed/W6NZfCO5SIk"
+        title="JavaScript Video"
+        allowFullScreen
+      />
+
+      <p>More materials coming soon 📘</p>
+    </div>
+  );
+}
