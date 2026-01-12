73cf984dc219ebeda4ff00d8d50a3289a2292903
Altynai <harmoni5413@gmail.com>
2026-01-12 10:22:08 -0500

    Added Python Fundamentals page

---
+++ learning-platform-frontend/src/pages/Python.tsx
@@ -0,0 +1,21 @@
+export default function Python() {
+  return (
+    <div>
+      <h2>Python Fundamentals</h2>
+
+      <iframe
+        width="600"
+        height="350"
+        src="https://www.youtube.com/embed/_uQrJ0TkZlc"
+        title="Python Video"
+        allowFullScreen
+      />
+
+      <p>
+        <a href="/docs/python-notes.pdf" target="_blank" rel="noreferrer">
+          Download Python Notes
+        </a>
+      </p>
+    </div>
+  );
+}
