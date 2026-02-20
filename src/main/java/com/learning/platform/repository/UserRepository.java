a69b7eea20a593be919e383213a08452dadd401b
Altynai <harmoni5413@gmail.com>
2026-02-20 13:48:33 -0500

    Updated UserRepository for database interactions

---
+++ src/main/java/com/learning/platform/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.learning.platform.repository;
+
+import com.learning.platform.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+}
