87254133e2003a63854218cbb661ded45b77f373
Altynai <harmoni5413@gmail.com>
2026-02-20 13:45:30 -0500

    Updated AppConfig.java for overall application configuration

--- src/main/java/com/learning/platform/config/AppConfig.java
+++ src/main/java/com/learning/platform/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.config;
+package com.learning.platform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
